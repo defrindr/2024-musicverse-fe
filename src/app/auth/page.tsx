01bab1934e2fd53de8d4927ced2d0045a1233030
Defri Indra Mahardika <defrindr@gmail.com>
2024-05-07 11:45:51 +0700

    default redirect

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,13 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+
 export default function AuthPage() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/auth/login');
+  }, [])
   return <></>;
 }
